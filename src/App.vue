11ab9eb7a093438f0c36b74848328649a33d0092
Lionel LECLERC <lionel.leclerc@dbschenker.com>
2026-02-16 13:46:55 +0100

    Syntaxe objet

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,29 @@
 <template>
-  <div :id="`p-${count}`">
-<div v-html="demoHtml"></div>    
-  Compteur {{ count }}
-  </div>
-  <div :style="{color:count >= 5 ? 'green' : 'red'}">
-  <div :class="{active: true}" v-if="count >= 5">Bravo vous avez incrémenté 5 fois ou plus</div>
-  <div :class="{active: false}" v-else="count < 5">Vous avez incrémenté moins de 5 fois</div>
-  </div>
-
-  <p>
-  <button @click="increment">Incrémenter</button>
-  <button @click="decrement">Décrémenter</button>
-  </p>
-
-  <hr>
-
-  <form action="" @submit.prevent="addMovie">
-    <input type="text" placeholder="Nouveau film" v-model="movieName"> {{ movieName }}
-    <button>Ajouter</button>
-  </form>
-
-  <p>
-    <button @click="sortMovies">Trier les films</button>
-  </p>
+  
   
   <ul>
-    <li v-for="movie in movies"
-    :key="movie">
-      {{ movie }} <button @click="deleteMovie(movie)">Supprimer</button>
-
-    </li>
+    <li>{{  person.firstName }}</li>
+    <li>{{  person.name }}</li>
+    <li>{{  person.age }}</li>
   </ul>
 
+  <button @click.prevent="randomAge">Changer age</button>
 </template>
 
 <script setup>
 import {ref} from 'vue'
 
-const count = ref(0)
-console.log(count, count.value)
-const demoHtml = '<span>Demo</span>'
-const movieName = ref('')
+const person = ref({
+  firstName: 'John',
+  name: 'Doe',
+  age: 20  
+})
 
-const movies = ref([
-  'Matrix',
-  'Lilo a Stitch',
-  'Titanic'
-])
-
-const increment = (event) => {
-  console.log(event)
-  count.value++
-}
-
-const decrement = () => {
-  count.value--
-}
-
-const deleteMovie = (movie) => {
-  movies.value = movies.value.filter(m => m != movie)
-}
-
-const sortMovies = () => {
-  movies.value.sort((a, b) => a > b ? 1 : -1)
-}
-
-const addMovie = (event) => {
-  movies.value.push(movieName.value)
-  movieName.value = ""
+const randomAge = () => {
+  person.value.age = Math.round(Math.random() * 100)
 }
 
 </script>
 
 <style>
-.active {
-  background-color: pink;
-}
 </style>
